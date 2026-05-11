f324e34bd66f84d32e6e8c37c3b22454aef1f5c7
Shaikh Aftabuddin <53236880+SkAftabuddin@users.noreply.github.com>
2026-05-11 18:02:52 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const http = require("http");
 
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("DevSecOps Demo Application Running");
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
